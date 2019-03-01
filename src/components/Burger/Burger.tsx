a9139306f0244257a5d40a7abe28c37a81eebfd9
Chethan Reddy <chethan.reddy@hired.com>
2019-03-01 12:32:52 -0700

    Add ingredients from state

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import styles from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = () => {
+interface Ingredients {
+  salad: number;
+  bacon: number;
+  cheese: number;
+  meat: number;
+  [key: string]: number;
+}
+
+interface Props {
+  ingredients: Ingredients;
+}
+
+const burger = (props: Props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient key={igKey + i} type={igKey} />
+    ));
+  });
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
